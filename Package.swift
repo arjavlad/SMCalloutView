258f2c75d57ee82b9b738450a469cdd7100e970a
Arjav Lad <arjavlad@users.noreply.github.com>
2021-09-08 13:25:20 +0530

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,15 @@
+// swift-tools-version:5.1
+import PackageDescription
+
+let package = Package(
+    name: "SMCalloutView",
+    platforms: [
+       .iOS(.v8)
+    ],
+    products: [
+        .library(name: "SMCalloutView", targets: ["SMCalloutView"])
+    ],
+    targets: [
+        .target(name: "SMCalloutView", dependencies: [], path: "/")
+    ]
+)
